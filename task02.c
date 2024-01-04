09ed98c65161bd532481cc3447fc82de08406e93
Mkayalai01 <mkayalai01@gmail.com>
2024-01-04 11:10:46 +0530

    added task02

---
+++ task02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Given A value:");
+     scanf("%d",&a);
+    if (a>0)
+    {
+    printf("This Number is Positive value ");
+   
+    }
+    else 
+    {
+     printf ("This Number is Negative value ");
+     
+    }
+} 
